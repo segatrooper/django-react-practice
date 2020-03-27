7eaa83117eb3aeed0ac16affc9e19317320aec93
BuildTools <unconfigured@null.spigotmc.org>
2020-03-27 11:35:00 -0700

    setup

--- main/home/views.py
+++ main/home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django import views
+from django.http import HttpResponse
 # Create your views here.
+
+class Home(views.View):
+    def get(self, request):
+        return HttpResponse('This is the home page with a GET request <h1> this is the greatest </h1>')
+    
+    
